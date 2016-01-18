8e379b0ab0304373e7f2586b97288885a0d3fc46
Reginaldo Marcelo dos Santos <reginaldosantos.br@gmail.com>
2016-01-18 20:42:48 -0200

    ClientController updated to work using Services and an external Validator

---
+++ app/Services/ClientService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: regin
+ * Date: 18/01/2016
+ * Time: 18:17
+ */
+
+namespace CodeProject\Services;
+
+
+class ClientService
+{
+
+}
---
+++ app/Validators/ClientValidator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: regin
+ * Date: 18/01/2016
+ * Time: 18:18
+ */
+
+namespace CodeProject\Validators;
+
+
+class ClientValidator
+{
+
+}
