396eb376fd6925f4f2b6148a4b9a47ad460ed625
Reginaldo Marcelo dos Santos <reginaldosantos.br@gmail.com>
2016-01-18 20:47:53 -0200

    Creating validators using an external lib

--- app/Validators/ClientValidator.php
+++ app/Validators/ClientValidator.php
@@ -9,7 +9,15 @@
 namespace CodeProject\Validators;
 
 
-class ClientValidator
-{
+use Prettus\Validator\LaravelValidator;
 
+class ClientValidator extends LaravelValidator
+{
+ protected $rules = [
+    'name'          => 'required|max:255',
+    'responsible'   => 'required|max:255',
+    'email'         => 'required|email',
+    'phone'         => 'required',
+    'address'       => 'required'
+    ];
 }
