51f235773b486447e6fce71bc03355c22ee27257
Reginaldo Santos <reginaldosantos.br@gmail.com>
2016-04-12 00:39:44 -0300

    environment setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
-var elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir'),
+    livereload = require('gulp-livereload'),
+    clean = require('gulp-clean'),
+    gulp = require('gulp')
 
+var config = {
+    assets_path:'./resources/assets',
+    build_path: './public/build'
+};
+
+config.bower_path = config.assets_path + '/../bower_components';
+
+config.build_path_js = config.bower_path + '/js';
+config.build_vendor_path_js = config.build_path_js + '/vendor';
+config.vendor_path_js = [
+    config.bower_path + '/jquery/dist/jquery.min.js',
+    config.bower_path + '/bootstrap/dist/js/bootstrap.min.js',
+    config.bower_path + '/angular/angular.min.js',
+    config.bower_path + '/angular-route/angular-route.min.js',
+    config.bower_path + '/angular-resource/angular-resource.min.js',
+    config.bower_path + '/angular-animate/angular-animate.min.js',
+    config.bower_path + '/angular-messages/angular-messages.min.js',
+    config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
+    config.bower_path + '/angular-strap/modules/navbar.min.js',
+];
+
+config.build_path_css = config.bower_path + '/css';
+config.build_vendor_path_css = config.build_path_css + '/vendor';
+config.vendor_path_css = [
+    config.bower_path + '/bootstrap/dist/css/bootstrap.min.css',
+    config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css',
+];
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
