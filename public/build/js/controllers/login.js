effd80e7cd1cffd31ee8170e4c8fc57d96cf23c4
Reginaldo Santos <reginaldosantos.br@gmail.com>
2016-06-04 23:04:56 -0300

    authentication & authorization

--- public/build/js/controllers/login.js
+++ public/build/js/controllers/login.js
@@ -2,7 +2,7 @@
  * Created by Reginaldo on 17/04/2016.
  */
 angular.module('app.controllers')
-.controller('loginController', ['$scope', function($scope){
+.controller('loginController', ['$scope', '$location', 'OAuth', function($scope, $location, OAuth){
     $scope.user  = {
         username: '',
             password: ''
@@ -10,5 +10,12 @@ angular.module('app.controllers')
 
     $scope.login = function(){
 
+        if($scope.formLogin.$valid){
+            OAuth.getAccessToken($scope.user).then(function(){
+                $location.path('home');
+            }, function(){
+                alert('Invalid credentials');
+            });
+        }
     };
 }]);
